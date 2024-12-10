5fad00e322dd9f800dbd3d3493075d8f16c31226
Alex Ameen <alex.ameen.tx@gmail.com>
2024-12-10 09:11:19 -0600

    day 5 part 1

--- day5_1.rb
+++ day5_1.rb
@@ -16,3 +16,33 @@ File.readlines('day5.in', chomp: true).each do |line|
   end
 end
 
+rules = {}
+
+for pair in pairs
+  if rules.has_key?(pair[1])
+    rules[pair[1]] << pair[0]
+  else
+    rules[pair[1]] = [pair[0]]
+  end
+end
+
+def is_valid(rules, update)
+  bads = []
+  for x in update
+    if bads.include?(x)
+      return false
+    end
+    if rules.has_key?(x)
+      bads += rules[x]
+    end
+  end
+  return true
+end
+
+sum = 0
+for update in updates
+  if is_valid(rules, update)
+    sum += update[(update.length/2).floor]
+  end
+end
+puts sum
