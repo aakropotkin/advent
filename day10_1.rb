df1ae6b38b760325ddcb4af2709815e3c6baf9fd
Alex Ameen <alex.ameen.tx@gmail.com>
2025-01-18 13:26:23 -0600

    fix robocop for day 10

--- day10_1.rb
+++ day10_1.rb
@@ -1,35 +1,60 @@
 #!/usr/bin/env ruby
 
+# frozen_string_literal: true
+
+# A class to represent a position on a map
+class Position
+  attr_accessor :row, :column
+
+  def initialize(row, column)
+    @row = row
+    @column = column
+  end
+
+  def to_s
+    "(#{@row}, #{@column})"
+  end
+
+  def to_a
+    [@row, @column]
+  end
+end
+
+# A topographical map
 class TopoMap
   attr_accessor :map
 
   def initialize(map)
     @map = map
-  end  # def initialize
-
-  def width()
-    return @map[0].length
   end
 
-  def height()
-    return @map.length
+  def width
+    @map[0].length
   end
 
-  def print_map()
-    for i in 0..(self.height()-1)
-      first = true
-      for j in 0..(self.width()-1)
-        if first
-          first = false
-        else
-          print ", "
-        end
-        print @map[i][j].to_s
+  def height
+    @map.length
+  end
+
+  def print_map
+    @map.each { |row| puts row.join(', ') }
+  end
+
+  def trailheads
+    trailheads = []
+    (0..(height - 1)).each do |i|
+      (0..(width - 1)).each do |j|
+        trailheads << Position.new(i, j) if @map[i][j].zero?
       end
-      puts
     end
-  end  # def print_map
-end  # class TopoMap
+    trailheads
+  end
+
+  def valid?(position)
+    (position.row >= 0) && (position.row < height) &&
+      (position.column >= 0) && (position.column < width)
+  end
+end
 
 map = File.readlines('day10.in', chomp: true).map { |l| l.chars.map(&:to_i) }
 topo_map = TopoMap.new(map)
