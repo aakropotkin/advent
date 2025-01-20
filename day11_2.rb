a21aa2de5728c859bd90acb438466b5cf720987d
Alex Ameen <alex.ameen.tx@gmail.com>
2025-01-20 11:32:14 -0600

    simplify day 11 part 2

--- day11_2.rb
+++ day11_2.rb
@@ -6,28 +6,25 @@ def split_integer(int)
   as_str = int.to_s
   return [int] if as_str.length.odd?
 
-  left_s = as_str[0..(as_str.length / 2) - 1]
-  right_s = as_str[(as_str.length / 2)..(as_str.length - 1)]
-  [left_s.to_i, right_s.to_i]
+  mid = as_str.length / 2
+  [as_str[0...mid].to_i, as_str[mid..-1].to_i]
 end
 
 MEMO = {}
 def expand(stone, times)
   return 1 if times.zero?
-
   return MEMO[[stone, times]] if MEMO.key?([stone, times])
 
-  if stone.zero?
-    result = expand(1, times - 1)
-  elsif stone.to_s.length.even?
-    split = split_integer(stone)
-    result = expand(split[0], times - 1) + expand(split[1], times - 1)
-  else
-    result = expand(stone * 2024, times - 1)
-  end
-  MEMO[[stone, times]] = result
-  result
+  MEMO[[stone, times]] =
+    if stone.zero?
+      expand(1, times - 1)
+    elsif stone.to_s.length.even?
+      split = split_integer(stone)
+      expand(split[0], times - 1) + expand(split[1], times - 1)
+    else
+      expand(stone * 2024, times - 1)
+    end
 end
 
-stones = File.readlines('day11.in', chomp: true)[0].split(' ').map(&:to_i)
-puts (stones.sum { |stone| expand(stone, 75) })
+stones = File.read('day11.in', chomp: true).split.map(&:to_i)
+puts stones.sum { |stone| expand(stone, 75) }
